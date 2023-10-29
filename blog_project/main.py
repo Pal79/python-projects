7bc42c9fdd63efe7ae099198dc62dfccbe30cff4
grunghar <pal.daniel.79@gmail.com>
2023-10-29 11:54:17 +0100

    flask request and smtlib added, create email_send def contact def modified

--- blog_project/main.py
+++ blog_project/main.py
@@ -1,9 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import requests
+import smtplib
+
+OWN_EMAIL = "pal.daniel.79@gmail.com"
+OWN_PASSWORD = "OWN_PASSWORD"
 
 API_ENDPOINT = "https://api.npoint.io/eb6cd8a5d783f501ee7d"
 posts = requests.get(API_ENDPOINT).json()
 #print(posts)
+PORT = 587
 
 app = Flask(__name__)
 
@@ -18,9 +23,24 @@ def home():
 def about():
     return render_template("about.html")
 
-@app.route("/contact")
+@app.route("/contact", methods=["GET","POST"])
 def contact():
-    return render_template("contact.html")
+    if request.method == "POST":
+        data = request.form
+        email_send(
+            data["name"],
+            data["email"],
+            data["phone"],
+            data["message"]
+        )
+        return render_template(
+            "contact.html",
+            msg_sent=True
+        )
+    return render_template(
+        "contact.html",
+        msg_sent=False
+    )
 
 @app.route("/post/<int:id>")
 def post(id):
@@ -32,6 +52,26 @@ def post(id):
         "post.html",
         post=requested_post
     )
+
+def email_send(name,email,phone,message):
+    email_message = f"Subject: New Message\n\n" \
+                    f"Name: {name}\n" \
+                    f"Email: {email}\n" \
+                    f"Phone: {phone}\n" \
+                    f"Message:\n" \
+                    f"{message}"
+    enc_msg = email_message.encode("UTF-8")
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+        connection.starttls()
+        connection.login(
+            user=OWN_EMAIL,
+            password=OWN_PASSWORD
+        )
+        connection.sendmail(
+            from_addr=OWN_EMAIL,
+            to_addrs=OWN_EMAIL,
+            msg=enc_msg
+        )
 
 if __name__ == "__main__":
     app.run(debug=True)
