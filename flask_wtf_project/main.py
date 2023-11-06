e93ad7a1b3b72adf9252a2f4a50e3b03c642fc92
grunghar <pal.daniel.79@gmail.com>
2023-11-06 16:52:44 +0100

    wtforms.validate added and login_form.validate_on_submit added

--- flask_wtf_project/main.py
+++ flask_wtf_project/main.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired
 
 '''
 Red underlines? Install the required packages first: 
@@ -27,9 +28,10 @@ app.secret_key = "ez egy titok"
 def home():
     return render_template('index.html')
 
-@app.route("/login")
+@app.route("/login", methods=["GET","POST"])
 def login():
     login_form = LoginForm()
+    login_form.validate_on_submit()
     return render_template(
         "login.html",
         form=login_form
