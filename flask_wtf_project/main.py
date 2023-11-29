c3772882a38a5bb421280fd62a8c5f86392f8f4e
grunghar <pal.daniel.79@gmail.com>
2023-11-29 08:20:17 +0100

    flask_bootstrap added

--- flask_wtf_project/main.py
+++ flask_wtf_project/main.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired
+from flask_bootstrap import Bootstrap5
 
 OWN_EMAIL = "pal.daniel.79@gmail.com"
 OWN_PASSWORD = "abc123"
